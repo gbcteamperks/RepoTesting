8a303ad07b64fa16267782ff28558da0656e3bbb
megazeroxzvk <vineet.z91@gmail.com>
2020-05-19 16:37:21 -0400

    Added roles of team members in comments below. Information taken from discord chat.

--- RepoTesting_Proj/RepoTesting_Main.cpp
+++ RepoTesting_Proj/RepoTesting_Main.cpp
@@ -8,3 +8,18 @@ int main()
 
 	return 0;
 }
+
+/*
+	ROLES ASSIGNED ----
+	taken from our discord channel.
+	BoobmandudeToday at 2:00 PM
+	@everyone Lead Programmer - Vineet
+	Gameplay - Julian
+	UI - Lucas
+	Producer - Joesph
+	TA - Dejan
+	Audio - Ammar
+	Design - Cole
+	General - Everyone - Bill
+
+*/
